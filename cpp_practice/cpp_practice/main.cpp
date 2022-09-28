9d25e259a81be28dfa2a09559049519281f89aa2
vu.dang <vu.dang@mesoneer.io>
2022-09-28 12:58:19 +0700

    Initialize C++ project

---
+++ cpp_practice/cpp_practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_practice
+//
+//  Created by Dang Tuan Vu on 28/09/2022.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
